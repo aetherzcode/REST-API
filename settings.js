ca8d0a80ff16c0d20ddfb78e9468fcb1fc8acfa1
aetherzcode <aetherzcode@gmail.com>
2024-10-17 13:34:09 +0700

    REST-API-AETHER

--- settings.js
+++ settings.js
@@ -1,39 +1,42 @@
 const fs = require('fs')
+require('dotenv').config()
 
-global.creator = 'AETHER'// yourname
-global.MONGO_DB_URI = "mongodb+srv://aetherz:Fahadd17@aetherz.ukjbb.mongodb.net/?retryWrites=true&w=majority&appName=AETHERz" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "aetherscode@gmail.com" //email
-global.email_password = "ihsynmfecsxgzmfr" //application password email
-global.limitCount = 10000
-global.YUOR_PORT = 8000
+global.creator = process.env.CREATOR || 'AETHER'
+global.MONGO_DB_URI = process.env.MONGO_DB_URI || process.env.MONGO_URL || "mongodb+srv://aetherz:Fahadd17@aetherz.ukjbb.mongodb.net/?retryWrites=true&w=majority&appName=AETHERz"
+global.ACTIVATION_TOKEN_SECRET = process.env.ACTIVATION_TOKEN_SECRET
+global.your_email = process.env.YOUR_EMAIL
+global.email_password = process.env.EMAIL_PASSWORD
+global.limitCount = parseInt(process.env.LIMIT_COUNT) || 10000
+global.YUOR_PORT = parseInt(process.env.YOUR_PORT) || 8000
+
 global.loghandler = {
-	noapikey:{
-		status: 403,
+    noapikey:{
+        status: 403,
         message: 'Input parameter apikey',
-        creator: `${creator}`,
+        creator: `${global.creator}`,
         result: "error"
     },
     error: {
         status: 503,
-        message: 'Service Unavaible, Sedang dalam perbaikan',
-        creator: `${creator}`
+        message: 'Service Unavailable, Sedang dalam perbaikan',
+        creator: `${global.creator}`
     },
     apikey: {
-    	status: 403,
-    	message: 'Forbiden, Invalid apikey',
-    	creator: `${creator}`
+        status: 403,
+        message: 'Forbidden, Invalid apikey',
+        creator: `${global.creator}`
     },
     noturl: {
-    	status: 403,
-    	message: 'Forbiden, Invlid url, masukkan parameter url',
-    	creator: `${creator}`,
+        status: 403,
+        message: 'Forbidden, Invalid url, masukkan parameter url',
+        creator: `${global.creator}`,
     }
 }
+
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(`Update'${__filename}'`)
-	delete require.cache[file]
-	require(file)
+    fs.unwatchFile(file)
+    console.log(`Update '${__filename}'`)
+    delete require.cache[file]
+    require(file)
 })
